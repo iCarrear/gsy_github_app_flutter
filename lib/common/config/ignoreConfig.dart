9585dcd24b8bd919b785175ec0811878430bcb45
Carrear <45230268+iCarrear@users.noreply.github.com>
2024-07-30 17:20:26 +0800

    Update ignoreConfig.dart

--- lib/common/config/ignoreConfig.dart
+++ lib/common/config/ignoreConfig.dart
@@ -1,5 +1,5 @@
 class NetConfig {
-  static const CLIENT_ID = "xxxx";
+  static const CLIENT_ID = "Ov23lirw3sIXttW3kZCt";
 
-  static const CLIENT_SECRET = "xxxxxxxxxxx";
+  static const CLIENT_SECRET = "aeaaccbbb7e7aa53f3d73a0fb8d20978f4e80c4f";
 }
